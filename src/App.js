e65d1472ac3675abc7772f1b70fac9fcaa4744e7
Gaia Guinouard-Balconi-Weida <gaiagoessocial@gmail.com>
2022-03-15 12:35:03 -0400

    added the about section

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import About from './components/About';
+import React from 'react';
+// import coverImage from "../../assets/cover/cover-image.jpg";
+import Nav from "./components/Nav";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// function App () {
+//   return (
+//     <section className="my-5">
+//     <h1 id="about">Who am I?</h1>
+//     <img src={coverImage} className="my-2" style={{width: "100%"}} alt="cover"/>
+//     </section>
+//   );
+// }
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav>
+
+      </Nav>
+      <main>
+        <About/>
+      </main>
     </div>
   );
 }
+
 
 export default App;
